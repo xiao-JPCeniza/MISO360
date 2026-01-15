ae7afa694167dab4d2ebdf3c00e94733daae2bb9
xiao-JPCeniza <cenizajp12@gmail.com>
2026-01-15 13:29:12 +0800

    first push

---
+++ resources/js/pages/settings/Appearance.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+
+import AppearanceTabs from '@/components/AppearanceTabs.vue';
+import HeadingSmall from '@/components/HeadingSmall.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import SettingsLayout from '@/layouts/settings/Layout.vue';
+import { edit } from '@/routes/appearance';
+import { type BreadcrumbItem } from '@/types';
+
+const breadcrumbItems: BreadcrumbItem[] = [
+    {
+        title: 'Appearance settings',
+        href: edit().url,
+    },
+];
+</script>
+
+<template>
+    <AppLayout :breadcrumbs="breadcrumbItems">
+        <Head title="Appearance settings" />
+
+        <h1 class="sr-only">Appearance Settings</h1>
+
+        <SettingsLayout>
+            <div class="space-y-6">
+                <HeadingSmall
+                    title="Appearance settings"
+                    description="Update your account's appearance settings"
+                />
+                <AppearanceTabs />
+            </div>
+        </SettingsLayout>
+    </AppLayout>
+</template>
---
+++ resources/js/pages/settings/Profile.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { Form, Head, usePage } from '@inertiajs/vue3';
+
+import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
+import DeleteUser from '@/components/DeleteUser.vue';
+import HeadingSmall from '@/components/HeadingSmall.vue';
+import InputError from '@/components/InputError.vue';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import AppLayout from '@/layouts/AppLayout.vue';
+import SettingsLayout from '@/layouts/settings/Layout.vue';
+import { edit } from '@/routes/profile';
+import { type BreadcrumbItem } from '@/types';
+
+interface Props {
+    status?: string;
+}
+
+defineProps<Props>();
+
+const breadcrumbItems: BreadcrumbItem[] = [
+    {
+        title: 'Profile settings',
+        href: edit().url,
+    },
+];
+
+const page = usePage();
+const user = page.props.auth.user;
+</script>
+
+<template>
+    <AppLayout :breadcrumbs="breadcrumbItems">
+        <Head title="Profile settings" />
+
+        <h1 class="sr-only">Profile Settings</h1>
+
+        <SettingsLayout>
+            <div class="flex flex-col space-y-6">
+                <HeadingSmall
+                    title="Profile information"
+                    description="Update your name and email address"
+                />
+
+                <Form
+                    v-bind="ProfileController.update.form()"
+                    class="space-y-6"
+                    v-slot="{ errors, processing, recentlySuccessful }"
+                >
+                    <div class="grid gap-2">
+                        <Label for="name">Name</Label>
+                        <Input
+                            id="name"
+                            class="mt-1 block w-full"
+                            name="name"
+                            :default-value="user.name"
+                            required
+                            autocomplete="name"
+                            placeholder="Full name"
+                        />
+                        <InputError class="mt-2" :message="errors.name" />
+                    </div>
+
+                    <div class="grid gap-2">
+                        <Label for="email">Email address</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            class="mt-1 block w-full"
+                            name="email"
+                            :default-value="user.email"
+                            required
+                            autocomplete="username"
+                            placeholder="Email address"
+                            disabled
+                        />
+                        <InputError class="mt-2" :message="errors.email" />
+                    </div>
+
+                    <div class="flex items-center gap-4">
+                        <Button
+                            :disabled="processing"
+                            data-test="update-profile-button"
+                            >Save</Button
+                        >
+
+                        <Transition
+                            enter-active-class="transition ease-in-out"
+                            enter-from-class="opacity-0"
+                            leave-active-class="transition ease-in-out"
+                            leave-to-class="opacity-0"
+                        >
+                            <p
+                                v-show="recentlySuccessful"
+                                class="text-sm text-neutral-600"
+                            >
+                                Saved.
+                            </p>
+                        </Transition>
+                    </div>
+                </Form>
+            </div>
+
+            <DeleteUser />
+        </SettingsLayout>
+    </AppLayout>
+</template>
